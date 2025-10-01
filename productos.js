52a8a64ce75ee952a0687b6007ff4244868bed19
reyfelipevlespana-netizen <reyfelipevlespana@gmail.com>
2025-10-01 18:26:02 +0200

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,66 @@
+const productos = [
+  {
+    nombre: "Lingote Oro 1KG 24K",
+    descripcion: "Lingote de oro puro 24K de inversión.",
+    
+    precioReal: 65000,
+    precioAmira: 8000,
+    imagen: "img/lingote1kg24k.jpg"
+  },
+  {
+    nombre: "Cadena Oro 18K",
+    descripcion: "Cadena de oro 18K, elegante y duradera.",
+    precioReal:39000 ,
+    precioAmira:3000 ,
+    imagen: "img/cadena18k.jpg"
+  },
+  {
+    nombre: "Anillo Oro 24K",
+    descripcion: "Anillo de oro puro 24K, ideal para ocasiones especiales.",
+    precioReal: 15000,
+    precioAmira: 2700,
+    imagen: "img/anillo24k.jpg"
+  },
+  {
+    nombre: "Pulsera Oro 18K",
+    descripcion: "Pulsera elegante de oro 18K, ideal para cualquier ocasión.",
+    precioReal: 9000,
+    precioAmira: 2000,
+    imagen: "img/pulsera18k.jpg"
+  },
+  {
+    nombre: "Aretes Oro 14K",
+    descripcion: "Aretes de oro 14K, perfectos para ocasiones especiales.",
+    precioReal: 4000,
+    precioAmira: 1000,
+    imagen: "img/aretes14k.jpg"
+  },
+  {
+    nombre: "Reloj Oro 18K",
+    descripcion: "Reloj de oro 18K, perfecto para coleccionistas y amantes del lujo.",
+    precioReal: 50000,
+    precioAmira: 6000,
+    imagen: "img/reloj18k.jpg"
+  },
+  {
+    nombre: "Collar Oro 22K",
+    descripcion: "Collar elegante de oro 22K, ideal para coleccionistas y eventos importantes.",
+    precioReal: 35000,
+    precioAmira: 4000,
+    imagen: "img/collar22k.jpg"
+  },
+  {
+    nombre: "Anillo Compromiso Oro 18K",
+    descripcion: "Anillo de compromiso en oro 18K, ideal para momentos inolvidables.",
+    precioReal: 13000,
+    precioAmira: 1500,
+    imagen: "img/anillo-compromiso18k.jpg"
+  },
+  {
+    nombre: "Pulsera Diamantes Oro 18K",
+    descripcion: "Pulsera de oro 18K con diamantes, ideal para eventos especiales y regalos de lujo.",
+    precioReal: 54000,
+    precioAmira: 4500,
+    imagen: "img/pulsera-diamantes18k.jpg"
+  }
+];
